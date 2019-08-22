7e74a9426d6ae8e81e38d941d40ed6abbdd81b0e
Kai Strüssmann <kstrussmann@craftingit.de>
2019-08-22 20:56:03 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,12 +10,20 @@ namespace gitRelease
         static void Main(string[] args)
         {
             Parser.Default.ParseArguments<githubOptions, giteaOptions>(args)
-               .WithParsed<githubOptions>(opts => RunOptionsAndReturnExitCode(opts));
+               .WithParsed<githubOptions>(opts => RunGithub(opts))
+               .WithParsed<giteaOptions>(opts => RunGitea(opts));
         }
 
-        private static void RunOptionsAndReturnExitCode(githubOptions opts)
+        private static void RunGithub(githubOptions opts)
         {
-            Console.WriteLine("Jej!");
+            Console.WriteLine("Github!");
+            Console.WriteLine(opts.Verbose);
+            Console.WriteLine(opts.Tag);
+        }
+
+        private static void RunGitea(giteaOptions opts)
+        {
+            Console.WriteLine("Gitea!");
             Console.WriteLine(opts.Verbose);
             Console.WriteLine(opts.Tag);
         }
