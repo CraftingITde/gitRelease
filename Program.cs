e9030339b74c5ec7e83c8af7b2834bdd54fa74b4
Kai Strüssmann <kstrussmann@craftingit.de>
2019-08-22 20:34:44 +0200

    Init Project

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using CommandLine;
+using gitRelease.Types;
+
+namespace gitRelease
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+               .WithParsed(opts => RunOptionsAndReturnExitCode(opts));
+        }
+
+        private static void RunOptionsAndReturnExitCode(Options opts)
+        {
+            Console.WriteLine("Jej!");
+            Console.WriteLine(opts.Verbose);
+        }
+    }
+}
---
+++ Types/Options.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using CommandLine;
+
+namespace gitRelease.Types
+{
+    public class Options
+    {
+        [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+        public bool Verbose { get; set; }
+    }
+}
