aba38c53d4cc1a2e2366b1664fc991db0812ed25
Markus <markus.krabbenhoeft@timbertec.com>
2019-10-23 13:10:22 +0200

    added GiTea support for GetType & addAtachmand
    Know Issue : gittea Update is not working

--- Commands/GiteaCommands.cs
+++ Commands/GiteaCommands.cs
@@ -6,6 +6,8 @@ using System.Text;
 namespace gitRelease.Commands
 {
     [Verb("gittea", HelpText = "Record changes to the repository.")]
+
+    [ChildVerbs(typeof(Update), typeof(Upload), typeof(GetTagType))]
     class GiteaCommands : BaseGitCommands
     {
 
@@ -14,7 +16,7 @@ namespace gitRelease.Commands
 
         [Option('h', HelpText = "use Http. Default is https")]
         public bool Http { get; set; } = false;
-        
+
         [Option('p', HelpText = "Port to use. Default is 443")]
         public int Port { get; set; } = 443;
 
@@ -27,12 +29,53 @@ namespace gitRelease.Commands
 
             public override void Execute()
             {
-            
+
                 var client = new Gitea.API.v1.Client(Token, Server, Port, !Http);
 
                 client.Repository.Release.addAttatchment(Owner, Repo, Tag, FileName);
 
             }
         }
+
+        [Verb("update", HelpText = "updating a Tag")]
+        public class Update : GiteaCommands
+        {
+
+            [Option('n', "Name", Required = false, HelpText = "The name vor the Release")]
+            public string Name { get; set; }
+
+            [Option('p', "Prerelease", Required = false, HelpText = "Prerelease?")]
+            public bool Prerelease { get; set; }
+
+            [Option('d', "Draft", Required = false, HelpText = "Draftrelease?")]
+            public bool DraftRelease { get; set; }
+
+            [Option('b', "Body", Required = true, HelpText = "The Release Body")]
+            public string Body { get; set; }
+
+            public override void Execute()
+            {
+
+                var client = new Gitea.API.v1.Client(Token, Server, Port, !Http);
+                client.Repository.Release.updateRelease(Owner, Repo, Tag, Body, Name, Prerelease, DraftRelease);
+
+            }
+        }
+        [Verb("getTagType", HelpText = "give the Type of the Tag")]
+        public class GetTagType : GiteaCommands
+        {
+
+            public override void Execute()
+            {
+
+                var client = new Gitea.API.v1.Client(Token, Server, Port, !Http);
+                var list = client.Repository.Release.Get(Owner, Repo);
+                var release = list.Find(r => r.Tag_name == this.Tag);
+                Console.WriteLine((release.Draft ? "Draftrelease" : "") + (release.Prerelease ? "Prerelease" : "") + (!release.Prerelease && !release.Draft ? "normalrelease" : ""));
+
+
+            }
+        }
+
     }
 }
--- Gitea.API/v1/Endpoints/ReleasesEndpoint.cs
+++ Gitea.API/v1/Endpoints/ReleasesEndpoint.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 using System.Web;
 using Gitea.API.v1.Types;
 using System.Globalization;
-using System.IO;
+using System.IO; 
 using System.Net.Http.Headers;
 
 namespace Gitea.API.v1.Endpoints
@@ -24,6 +24,7 @@ namespace Gitea.API.v1.Endpoints
             var job = addAttatchmentAsync(owner, name, tag, filename);
             job.Wait();
         }
+
 
 
         public async Task addAttatchmentAsync(string owner, string name, string tag, string filename)
@@ -60,20 +61,67 @@ namespace Gitea.API.v1.Endpoints
 
             using (var rest = Client.CreateBaseClient())
             {
-                var resp = await rest.GetAsync("repos/" + HttpUtility.UrlEncode(owner) + "/" + HttpUtility.UrlEncode(name) + "/releases");
+                Console.WriteLine("GetAsync base Adress : "+  rest.BaseAddress);
+                Console.WriteLine("GetAsync Name : " + name);                  
+                Console.WriteLine("GetAsync owner : " + owner);                  
+
+
+                string request = "repos/" + HttpUtility.UrlEncode(owner) + "/" + HttpUtility.UrlEncode(name) + "/releases";
+                 
+                var resp = await rest.GetAsync(request);
                 await CheckResponse(resp);
 
-                var json = await resp.Content.ReadAsStringAsync();
-
+                var json = await resp.Content.ReadAsStringAsync();                 
                 var repo = JsonConvert.DeserializeObject<List<Release>>
                     (
                        json
                     );
 
+
                 return repo;
 
             }
         }
+
+        public void updateRelease(string owner, string repo, string TagName, string Body, string NewName = null, bool Prerelease = false, bool Draftrelease = false)
+        {
+
+            Console.WriteLine("IchwarHier"); 
+            var job = updateReleaseAsync(owner, repo, TagName, Body, NewName, Prerelease, Draftrelease );
+            job.Wait();
+
+        }
+
+        public async Task updateReleaseAsync (string Owner, string Name,  string TagName, string Body, string NewName = null, bool Prerelease = false, bool Draftrelease = false)
+        {
+            var release = this.Get(Owner, Name).Find(r => r.Tag_name == TagName);             
+            Console.WriteLine("updateReleaseAsync release ID: " + release.Id.ToString());
+            using (var rest = Client.CreateBaseClient())
+            {
+                Release release1 = new Release();
+                release1.Draft = Draftrelease;
+                release1.Body = Body;
+                release1.Name = NewName;
+                release1.Prerelease = Prerelease;
+                release1.Tag_name = TagName;
+
+                var json = JsonConvert.SerializeObject(release1);
+                Console.WriteLine("updatereleasesAsync json: " + json);
+
+                var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+                string request = "repos/" + HttpUtility.UrlEncode(Owner) + "/" + HttpUtility.UrlEncode(Name) + "/releases/" + HttpUtility.UrlEncode(release.Id.ToString());
+
+                Console.WriteLine("updateReleaseAsync Request: " + request);                
+                var resp = await rest.PostAsync(request, httpContent);            
+                 
+                await CheckResponse(resp);
+                Console.WriteLine("updateReleaseAsync after Post: it's done !!!");
+            }
+        }
+
+            
+
+                
 
     }
 }
